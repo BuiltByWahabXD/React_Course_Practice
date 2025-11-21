3a440d68b2717051755020bdb0d3911b17d39d8b
Abdul Wahab <93056444+BuiltByWahabXD@users.noreply.github.com>
2025-11-21 18:21:55 +0500

    Added Some Instructions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import ArrayUpdatorObject from "./UpdateArrayInObject/ArrayUpdatorObject.jsx";
+//You need to import the component you want to render in App.jsx 
+// For example, if you want to render the ArrayUpdatorObject component, you would do it like this:
+//import ArrayUpdatorObject from "./UpdateArrayInObject/ArrayUpdatorObject.jsx";
+//Then, you can use that component inside the App function as shown below:
+//return (
+//    <>  
+//      <ArrayUpdatorObject/>
+//    </>
+//  );
+
 function App() {
 
      
